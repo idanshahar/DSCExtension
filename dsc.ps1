258530e8d5e7e770f7fae5e42bbdc2f765c5a882
Administrator <admin@example.com>
2018-08-14 22:14:37 +0300

    init

---
+++ dsc.ps1
@@ -0,0 +1,16 @@
+Configuration ScriptTest
+{
+    Import-DscResource –ModuleName 'PSDesiredStateConfiguration'
+
+    Script ScriptExample
+    {
+        SetScript =
+        {
+            $sw = New-Object System.IO.StreamWriter("C:\TempFolder\TestFile.txt")
+            $sw.WriteLine("Some sample string")
+            $sw.Close()
+        }
+        TestScript = { Test-Path "C:\TempFolder\TestFile.txt" }
+        GetScript = { @{ Result = (Get-Content C:\TempFolder\TestFile.txt) } }
+    }
+}
